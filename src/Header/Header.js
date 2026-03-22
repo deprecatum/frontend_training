7310d8b0b242a2cee5eb538371eb75ef9c762f5c
deprecatum <deprecatum@gmail.com>
2026-03-22 17:32:18 +0000

    Header example

---
+++ src/Header/Header.js
@@ -0,0 +1,10 @@
+
+export const Header = () => {
+    return (
+        <div>
+            <div>Button1</div>
+            <div>Button2</div>
+            <div>Button3</div>
+        </div>
+    )
+}
