2180fd0d131bd021e7e82b21e0975a95cedb0318
gornalo <goncaloluz84@gmail.com>
2026-04-02 14:58:44 +0100

    header

--- src/page/App.js
+++ src/page/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <img src="logo512.png" alt="Italian Trulli" height="40" width="40"></img>
+      <div>Item 1</div>
+  <div>Item 2</div>
+  <div>Item 3</div>  
+  <div>Item 4</div>
+  <div>Item 5</div>
       </header>
     </div>
   );
