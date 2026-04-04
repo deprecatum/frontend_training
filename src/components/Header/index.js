b5d58ffb216e73675d2a49d012efb20fd0ca3428
deprecatum <deprecatum@gmail.com>
2026-04-04 14:24:18 +0100

    Moved header to its own component

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+export const Header = () => {
+    return (
+      <header className='header'>
+        <img src="logo192.png" className="logo" alt="logo" />
+        <div className='container'>
+          <div>Item 1</div>
+          <div>Item 2</div>
+          <div>Item 3</div>
+          <div>Item 4</div>
+          <div>Item 5</div>
+        </div>
+      </header>
+    )
+}
