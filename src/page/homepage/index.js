73aa25add90e9d5291b951ade5a929d39160d2d6
deprecatum <33914332+deprecatum@users.noreply.github.com>
2026-04-04 14:28:52 +0100

    Moved header to its own component (#4)

--- src/page/homepage/index.js
+++ src/page/homepage/index.js
@@ -1,18 +1,10 @@
 import './homepage.css';
+import { Header } from '../../components/Header';
 
 export function Homepage() {
   return (
     <div className="homepage">
-      <header className='header'>
-        <img src="logo192.png" className="logo" alt="logo" />
-        <div className='container'>
-          <div>Item 1</div>
-          <div>Item 2</div>
-          <div>Item 3</div>
-          <div>Item 4</div>
-          <div>Item 5</div>
-        </div>
-      </header>
+      <Header />
     </div>
   );
 }
