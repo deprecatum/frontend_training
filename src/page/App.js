eace0858084cad5d27990c683ce8eedc2a46df1b
gornalo <goncaloluz84@gmail.com>
2026-04-09 23:41:46 +0100

    header

--- src/page/App.js
+++ src/page/App.js
@@ -6,11 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
        <img src="logo512.png" alt="Italian Trulli" height="40" width="40"></img>
-      <div>Item 1</div>
-  <div>Item 2</div>
-  <div>Item 3</div>  
-  <div>Item 4</div>
-  <div>Item 5</div>
+        <items className="App-items">
+         <div>Item 1</div><div>Item 2</div><div>Item 3</div>  <div>Item 4</div><div>Item 5</div>
+  </items>
       </header>
     </div>
   );
